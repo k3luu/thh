ca5557370446d835a58845516b03520987c6f6d9
Kathy Luu <kathyluu820@gmail.com>
2018-05-10 11:22:58 -0700

    updated the landing page with an overlay on the background image

--- src/layouts/MainHeader/MainHeader.jsx
+++ src/layouts/MainHeader/MainHeader.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import classNames from "classnames";
-import "./MainHeader.css";
+import React from 'react';
+import classNames from 'classnames';
+import './MainHeader.css';
 
 class MainHeader extends React.Component {
   render() {
     const { children, cover } = this.props;
 
-    const classes = classNames("main-header", this.props.className, {
-      "no-cover": !cover
+    const classes = classNames('main-header arrow-radial', this.props.className, {
+      'no-cover': !cover
     });
 
     const getStyle = () => {
