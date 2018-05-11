0969db395888f513864596c16fbf1c90f945ce6d
Kathy Luu <kathyluu820@gmail.com>
2018-05-11 15:09:40 -0700

    Refactored ambassadors for easy control

--- src/components/NoMatterTheMountain/NoMatterTheMountain.jsx
+++ src/components/NoMatterTheMountain/NoMatterTheMountain.jsx
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import './NoMatterTheMountain.css';
+import ambassadors from './ambassadors';
 
 class NoMatterTheMountain extends Component {
+  handleAmbassadorTitle(list) {
+    let titles = '';
+
+    list.map(item => {
+      if (titles.length > 1) titles += ' | ';
+      titles += item;
+    });
+
+    return titles;
+  }
+
+  handleAmbassadorsDisplay() {
+    return ambassadors.map(p => (
+      <div key={p.id}>
+        <h2>{p.name}</h2>
+        <h5>{this.handleAmbassadorTitle(p.title)}</h5>
+        <p>{p.bio}</p>
+        <p>
+          <i className="fa fa-instagram" /> {p.instagram}
+        </p>
+      </div>
+    ));
+  }
+
   // Edit About component or pages/about.jsx to include your information.
   render() {
     return (
@@ -27,63 +52,7 @@ class NoMatterTheMountain extends Component {
         <h2>Meet Our Campaign Ambassadors</h2>
         <p>Read up on our campaign ambassadors and how they conquer their own mountains!</p>
 
-        <h3>France Marie</h3>
-        <h5>Pediatric intensive care Nurse | Outdoor Enthusiast</h5>
-        <p>
-          Growth does not exist without stepping outside your comfort zone. I picked up snowboarding a few seasons ago.
-          Somewhat similar to other challenges I have dealt with in life, I just had to get back up after falling and
-          keep at it. I push myself to ride different terrain, try things that I know would make me fall, and ride with
-          others better than myself. Self-doubt is not a product of physical inability, but mental inability. Fear and
-          doubt limits and originates in the mind. I never compare myself to others nor do I strive for perfection,
-          rather, my goal is to be better than the last time I road. Stay motivated, optimistic, and true to yourself.
-          Create the grandest and highest vision for yourself because you are what you believe.
-        </p>
-        <p>Instagram | @francemarie_</p>
-
-        <h3>Francis Kenneth</h3>
-        <h5>Hyper-Creative | Influencer-Brand Manager</h5>
-        <p>
-          FAILURE. I don't have any natural born talents or physical gifts. I've been an underdog my whole life (I'd
-          argue that I still am), but I've been able to get up and over the top through plenty of trial and error. It's
-          through those times that I was able to learn and grow. By failing over and over and over again, I'm able to
-          succeed through and with experience. You can't have failure without success. You can't have success without
-          failure.
-        </p>
-        <p>Instagram | @francemarie_</p>
-
-        <h3>Lawrence Coronel</h3>
-        <h5>Fitness & Lifestyle Influencer</h5>
-        <p>
-          I relate my life to my passion for fitness. In many ways they are similar to that moment you open the gym
-          door. You are suited and ready to take on a task that seems impossible to others, a lift that seems immovable.
-          The odds are stacked against you. However, it is in your mindset that the real battle happens. It’s you who
-          decides whether you accept defeat or overcome that fear and climb that mountain. It is in each and everyone of
-          us built, our DNA, to adapt and overcome our obstacles. We will face challenges everyday but know you have it
-          in you to make a change and climb.
-        </p>
-        <p>Instagram | @ob3ydalaw</p>
-
-        <h3>Tiffany Huang</h3>
-        <h5>Transportation technology Engineer | Climber</h5>
-        <p>
-          I learned from climbing that power can emerge from perseverance, planning, and practice. I learned to not fear
-          failure. I learned that confidence in myself can allow me to do things I never imagined possible. I can
-          conquer mountains if I know I can.
-        </p>
-        <p>Instagram | @yihsintiffany</p>
-
-        <h3>Scottie Nguyen</h3>
-        <h5>Salt & Fresh Water Fisherman</h5>
-        <p>
-          Make every decision, a conscious decision. From the culture of Fishing I've learned the importance of
-          preparation, adaptation, and having a commitment to persevere. Every trip offshore is a new mountain with new
-          conditions. Often times we only have short windows to produce. Hours of preparation for 15 minutes of
-          opportunity. In these short windows my every decision makes the difference. I don't always bring a catch home,
-          but I never come home empty handed. Learning what worked, and what didn't, to change my decisions at the next
-          opportunity. In life, you can't move the mountain or control its rock slides, but you choose your path to
-          conquering it.
-        </p>
-        <p>Instagram | @fishwrecked</p>
+        {this.handleAmbassadorsDisplay()}
       </div>
     );
   }
---
+++ src/components/NoMatterTheMountain/ambassadors.js
@@ -0,0 +1,39 @@
+const ambassadors = [
+  {
+    "id": 0,
+    "name": "France Marie",
+    "title": ["Pediatric Intensive Care Nurse", "Outdoor Enthusiast"],
+    "bio": "Growth does not exist without stepping outside your comfort zone. I picked up snowboarding a few seasons ago. Somewhat similar to other challenges I have dealt with in life, I just had to get back up after falling and keep at it. I push myself to ride different terrain, try things that I know would make me fall, and ride with others better than myself. Self-doubt is not a product of physical inability, but mental inability. Fear and doubt limits and originates in the mind. I never compare myself to others nor do I strive for perfection, rather, my goal is to be better than the last time I road. Stay motivated, optimistic, and true to yourself. Create the grandest and highest vision for yourself because you are what you believe.",
+    "instagram": "francemarie_"
+  },
+    {
+        "id": 1,
+        "name": "Francis Kenneth",
+        "title": ["Hyper-Creative", "Influencer-Brand Manager"],
+        "bio": "FAILURE. I don't have any natural born talents or physical gifts. I've been an underdog my whole life (I'd argue that I still am), but I've been able to get up and over the top through plenty of trial and error. It's through those times that I was able to learn and grow. By failing over and over and over again, I'm able to succeed through and with experience. You can't have failure without success. You can't have success without failure.",
+        "instagram": "franciskenneth"
+    },
+    {
+        "id": 2,
+        "name": "Lawrence Coronel",
+        "title": ["Fitness & Lifestyle Influencer"],
+        "bio": "I relate my life to my passion for fitness. In many ways they are similar to that moment you open the gym door. You are suited and ready to take on a task that seems impossible to others, a lift that seems immovable. The odds are stacked against you. However, it is in your mindset that the real battle happens. It’s you who decides whether you accept defeat or overcome that fear and climb that mountain. It is in each and everyone of us built, our DNA, to adapt and overcome our obstacles. We will face challenges everyday but know you have it in you to make a change and climb.",
+        "instagram": "ob3ydalaw"
+    },
+    {
+        "id": 3,
+        "name": "Tiffany Huang",
+        "title": ["Transportation Technology Engineer", "Climber"],
+        "bio": "I learned from climbing that power can emerge from perseverance, planning, and practice. I learned to not fear failure. I learned that confidence in myself can allow me to do things I never imagined possible. I can conquer mountains if I know I can.",
+        "instagram": "yihsintiffany"
+    },
+    {
+        "id": 4,
+        "name": "Scottie Nguyen",
+        "title": ["Salt & Fresh Water Fisherman"],
+        "bio": "Make every decision, a conscious decision. From the culture of Fishing I've learned the importance of preparation, adaptation, and having a commitment to persevere. Every trip offshore is a new mountain with new conditions. Often times we only have short windows to produce. Hours of preparation for 15 minutes of opportunity. In these short windows my every decision makes the difference. I don't always bring a catch home, but I never come home empty handed. Learning what worked, and what didn't, to change my decisions at the next opportunity. In life, you can't move the mountain or control its rock slides, but you choose your path to conquering it.",
+        "instagram": "fishwrecked"
+    }
+];
+
+export default ambassadors;
