1a86ff228effbb0ba544cb21146936d5e7d194fe
Kathy Luu <kathyluu820@gmail.com>
2018-05-17 12:04:52 -0700

    social media icons replaced with font awesome

--- src/components/SocialMediaIcons/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
-import "./SocialMediaIcons.css";
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import './SocialMediaIcons.css';
 
 class SocialMediaIcons extends React.Component {
   render() {
@@ -8,15 +8,27 @@ class SocialMediaIcons extends React.Component {
     if (urls && urls.length > 0) {
       return (
         <div className="social-media-icons">
-          {urls.map(url => (
-            <SocialIcon
-              key={url}
-              className="social-media-icon"
-              url={url}
-              color={color}
-              style={{ height: null, width: null }}
-            />
-          ))}
+          {/*{urls.map(url => (*/}
+          {/*<SocialIcon*/}
+          {/*key={url}*/}
+          {/*className="social-media-icon"*/}
+          {/*url={url}*/}
+          {/*color={color}*/}
+          {/*style={{ height: null, width: null }}*/}
+          {/*/>*/}
+          {/*))}*/}
+          <a href={urls[0]} target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-youtube" />
+          </a>
+          <a href={urls[1]} target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-facebook" />
+          </a>
+          <a href={urls[2]} target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-instagram" />
+          </a>
+          <a href={urls[3]} target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-twitter" />
+          </a>
         </div>
       );
     }
