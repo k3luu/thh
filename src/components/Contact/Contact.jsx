b76f9181b20c2c5d65b5ef35b09451aa97f4d784
Kathy Luu <kathyluu820@gmail.com>
2018-05-24 21:40:09 -0700

    added followup route

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ class Events extends Component {
   render() {
     return (
       <div className="main-content contact">
-        <form className="contact-form" name="contact" method="POST" data-netlify="true">
+        <form className="contact-form" name="contact" method="POST" data-netlify="true" action="/">
           <Box display="flex" className="contact-fields">
             <Box column={6} marginRight={2}>
               <TextField className="contact-field" type="text" name="name" id="name" placeholder="Name" />
