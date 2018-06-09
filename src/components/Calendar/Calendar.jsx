c45aca5d9b0f00335c3c3163551a0db9d8a8d9e1
Kathy Luu <kathyluu820@gmail.com>
2018-06-08 17:49:50 -0700

    manual changes to prism js package

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,8 +1,116 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './Calendar.css';
+
+BigCalendar.momentLocalizer(moment);
 
 const GOOGLE_API_KEY = 'AIzaSyAy-Hn-2rmG7nmc2etp_hNdbLe_xFkpygw',
   CALENDAR_ID = 'r511go4f29gmulloe8fmh5ts8s@group.calendar.google.com';
+
+const calEvents = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1)
+  },
+  {
+    id: 1,
+    title: 'Long Event',
+    start: new Date(2015, 3, 7),
+    end: new Date(2015, 3, 10)
+  },
+
+  {
+    id: 2,
+    title: 'DTS STARTS',
+    start: new Date(2016, 2, 13, 0, 0, 0),
+    end: new Date(2016, 2, 20, 0, 0, 0)
+  },
+
+  {
+    id: 3,
+    title: 'DTS ENDS',
+    start: new Date(2016, 10, 6, 0, 0, 0),
+    end: new Date(2016, 10, 13, 0, 0, 0)
+  },
+
+  {
+    id: 4,
+    title: 'Some Event',
+    start: new Date(2015, 3, 9, 0, 0, 0),
+    end: new Date(2015, 3, 9, 0, 0, 0)
+  },
+  {
+    id: 5,
+    title: 'Conference',
+    start: new Date(2015, 3, 11),
+    end: new Date(2015, 3, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    id: 6,
+    title: 'Meeting',
+    start: new Date(2015, 3, 12, 10, 30, 0, 0),
+    end: new Date(2015, 3, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    id: 7,
+    title: 'Lunch',
+    start: new Date(2015, 3, 12, 12, 0, 0, 0),
+    end: new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    id: 8,
+    title: 'Meeting',
+    start: new Date(2015, 3, 12, 14, 0, 0, 0),
+    end: new Date(2015, 3, 12, 15, 0, 0, 0)
+  },
+  {
+    id: 9,
+    title: 'Happy Hour',
+    start: new Date(2015, 3, 12, 17, 0, 0, 0),
+    end: new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    id: 10,
+    title: 'Dinner',
+    start: new Date(2015, 3, 12, 20, 0, 0, 0),
+    end: new Date(2015, 3, 12, 21, 0, 0, 0)
+  },
+  {
+    id: 11,
+    title: 'Birthday Party',
+    start: new Date(2015, 3, 13, 7, 0, 0),
+    end: new Date(2015, 3, 13, 10, 30, 0)
+  },
+  {
+    id: 12,
+    title: 'Late Night Event',
+    start: new Date(2015, 3, 17, 19, 30, 0),
+    end: new Date(2015, 3, 18, 2, 0, 0)
+  },
+  {
+    id: 13,
+    title: 'Multi-day Event',
+    start: new Date(2015, 3, 20, 19, 30, 0),
+    end: new Date(2015, 3, 22, 2, 0, 0)
+  },
+  {
+    id: 14,
+    title: 'Today',
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3))
+  }
+];
+
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
 class Calendar extends Component {
   state = {
@@ -10,7 +118,7 @@ class Calendar extends Component {
   };
 
   componentDidMount() {
-    this.getEvents();
+    // this.getEvents();
     // setInterval(() => {
     //   this.getEvents();
     // }, 60000);
@@ -80,10 +188,14 @@ class Calendar extends Component {
     });
 
     return (
-      <div>
-        <h1>hi</h1>
-        {events.length > 0 && eventsList}
-      </div>
+      <BigCalendar
+        popup
+        events={calEvents}
+        views={allViews}
+        step={60}
+        showMultiDayTimes
+        defaultDate={new Date(2015, 3, 1)}
+      />
     );
   }
 }
