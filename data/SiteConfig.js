590b1f369c9db9401198a86e638f80fef5b32df9
Kathy Luu <kathyluu820@gmail.com>
2018-06-12 11:34:29 -0700

    google analytics id

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -16,7 +16,7 @@ module.exports = {
   siteRssAuthor: 'Casper User', // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
   sitePaginationLimit: 10, // The max number of posts per page.
-  // googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
+  googleAnalyticsID: "UA-120716989-1", // GA tracking ID.
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
     'https://www.youtube.com/channel/UCDMdw-H9j9H5tlQy3jUv5rQ',
