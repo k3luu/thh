6e717ae66675a2e97b1971fbf6ff227e65fe300f
Kathy Luu <kathyluu820@gmail.com>
2018-06-27 15:12:24 -0700

    ambassador images

--- src/components/NoMatterTheMountain/ambassadors/ambassadors.js
+++ src/components/NoMatterTheMountain/ambassadors/ambassadors.js
@@ -7,7 +7,7 @@ const ambassadors = [
       'Growth does not exist without stepping outside your comfort zone. I picked up snowboarding a few seasons ago. Somewhat similar to other challenges I have dealt with in life, I just had to get back up after falling and keep at it. I push myself to ride different terrain, try things that I know would make me fall, and ride with others better than myself. Self-doubt is not a product of physical inability, but mental inability. Fear and doubt limits and originates in the mind. I never compare myself to others nor do I strive for perfection, rather, my goal is to be better than the last time I road. Stay motivated, optimistic, and true to yourself. Create the grandest and highest vision for yourself because you are what you believe.',
     instagram: 'francemarie_',
     photo_src:
-      'https://static1.squarespace.com/static/56d00ad1e32140703d5d8f91/t/59e3e51e9f8dcec5944daa6d/1508107556965/FA.jpg'
+      'https://s3-us-west-1.amazonaws.com/twohalfhitches/persons/FA.jpg'
   },
   {
     id: 1,
@@ -17,7 +17,7 @@ const ambassadors = [
       "FAILURE. I don't have any natural born talents or physical gifts. I've been an underdog my whole life (I'd argue that I still am), but I've been able to get up and over the top through plenty of trial and error. It's through those times that I was able to learn and grow. By failing over and over and over again, I'm able to succeed through and with experience. You can't have failure without success. You can't have success without failure.",
     instagram: 'franciskenneth',
     photo_src:
-      'https://static1.squarespace.com/static/56d00ad1e32140703d5d8f91/t/59dc3080f5e2317170f2635c/1507602777337/_J8A3502.jpg'
+      'https://s3-us-west-1.amazonaws.com/twohalfhitches/persons/_J8A3502.jpg'
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ const ambassadors = [
       'I relate my life to my passion for fitness. In many ways they are similar to that moment you open the gym door. You are suited and ready to take on a task that seems impossible to others, a lift that seems immovable. The odds are stacked against you. However, it is in your mindset that the real battle happens. It’s you who decides whether you accept defeat or overcome that fear and climb that mountain. It is in each and everyone of us built, our DNA, to adapt and overcome our obstacles. We will face challenges everyday but know you have it in you to make a change and climb.',
     instagram: 'ob3ydalaw',
     photo_src:
-      'https://static1.squarespace.com/static/56d00ad1e32140703d5d8f91/t/59ea5dd6dc2b4a175abdcd5b/1508532121340/_J8A3739.jpg?'
+      'https://s3-us-west-1.amazonaws.com/twohalfhitches/persons/_J8A3739.jpg'
   },
   {
     id: 3,
@@ -37,7 +37,7 @@ const ambassadors = [
       'I learned from climbing that power can emerge from perseverance, planning, and practice. I learned to not fear failure. I learned that confidence in myself can allow me to do things I never imagined possible. I can conquer mountains if I know I can.',
     instagram: 'yihsintiffany',
     photo_src:
-      'https://static1.squarespace.com/static/56d00ad1e32140703d5d8f91/t/59ea60148fd4d2c5083be0c1/1508532256290/IMG_2861b.jpg'
+      'https://s3-us-west-1.amazonaws.com/twohalfhitches/persons/IMG_2861b.jpg'
   },
   {
     id: 4,
@@ -47,7 +47,7 @@ const ambassadors = [
       "Make every decision, a conscious decision. From the culture of Fishing I've learned the importance of preparation, adaptation, and having a commitment to persevere. Every trip offshore is a new mountain with new conditions. Often times we only have short windows to produce. Hours of preparation for 15 minutes of opportunity. In these short windows my every decision makes the difference. I don't always bring a catch home, but I never come home empty handed. Learning what worked, and what didn't, to change my decisions at the next opportunity. In life, you can't move the mountain or control its rock slides, but you choose your path to conquering it.",
     instagram: 'fishwrecked',
     photo_src:
-      'https://static1.squarespace.com/static/56d00ad1e32140703d5d8f91/t/59ea5d7c64b05fb185589ea9/1508534676830/IMG_2888.jpg'
+      'https://s3-us-west-1.amazonaws.com/twohalfhitches/persons/IMG_2888.jpg'
   }
 ];
 
