4f14eec5ec36af9c9cd4d6eda3587a2dbde762fc
Kathy Luu <kathyluu820@gmail.com>
2018-07-11 15:27:27 -0700

    added loading spinner until we get calendar data

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -180,8 +180,57 @@ class Calendar extends Component {
   }
 
   render() {
-    const { events } = this.state;
+    const { events, isLoading } = this.state;
     console.log('events', events);
+
+    if (isLoading)
+      return (
+        <div className="spinner__container">
+          <div className="spinner">
+            <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+              <circle
+                className="length"
+                fill="none"
+                strokeWidth="8"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="28"
+              />
+            </svg>
+            <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+              <circle
+                fill="none"
+                strokeWidth="8"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="28"
+              />
+            </svg>
+            <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+              <circle
+                fill="none"
+                strokeWidth="8"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="28"
+              />
+            </svg>
+            <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+              <circle
+                fill="none"
+                strokeWidth="8"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="28"
+              />
+            </svg>
+          </div>
+        </div>
+      );
 
     return (
       <div>
