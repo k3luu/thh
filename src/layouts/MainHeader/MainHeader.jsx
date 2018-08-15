ef6c38fc9afbbb44ea6a5ad71f67d0d665ef6c00
Kathy Luu <kathyluu820@gmail.com>
2018-08-14 23:20:34 -0700

    using gestalt image component for header

--- src/layouts/MainHeader/MainHeader.jsx
+++ src/layouts/MainHeader/MainHeader.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import classNames from 'classnames';
-import './MainHeader.css';
+import React from "react";
+import { Box, Image } from "gestalt";
+import classNames from "classnames";
+import "./MainHeader.css";
 
 class MainHeader extends React.Component {
   render() {
     const { children, cover } = this.props;
 
     const classes = classNames(
-      'main-header arrow-radial',
+      "main-header arrow-radial",
       this.props.className,
       {
-        'no-cover': !cover
+        "no-cover": !cover
       }
     );
 
-    const getStyle = () => {
-      if (cover) {
-        return { backgroundImage: `url("${cover}")` };
-      }
-      return null;
-    };
-
     return (
-      <header className={classes} style={getStyle()}>
-        {children}
+      <header className={classes}>
+        <Box height="100vh" width="100%">
+          <Image
+            alt="Header"
+            naturalHeight={1}
+            naturalWidth={1}
+            fit="cover"
+            src={cover}
+            color="#172121"
+          >
+            {children}
+          </Image>
+        </Box>
       </header>
     );
   }
